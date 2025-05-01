7f130e12ba5a00ea1785b4b5acd8478f609b9f9e
Sadat Mazumder <Sadat.Mazumder@dwer.wa.gov.au>
2025-05-01 15:49:12 +1000

    added DTO for new format specification

---
+++ src/ecc-pricing/function/dulux.integration.ecc.models/Request/EccPricingRequest.cs
@@ -0,0 +1,197 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dulux.integration.ecc.models.Request
+{
+    public class EsSalesOrderDataSet
+    {
+        [JsonProperty("results")]
+        public List<ResultsEsSalesOrderDataSet> resultsEsSalesOrderDataSet;
+    }
+
+    public class ItOrderItemsSet
+    {
+        [JsonProperty("results")]
+        public List<ResultsItOrderItemsSet> resultsItOrderItemsSet;
+    }
+
+    public class ItOrderPartnersSet
+    {
+        [JsonProperty("results")]
+        public List<ResultsItOrderPartnersSet> resultsItOrderPartnersSet;
+    }
+
+    public class ItOrderSchedLinesSet
+    {
+        [JsonProperty("results")]
+        public List<ResultsItOrderSchedLinesSet> resultsItOrderSchedLinesSet;
+    }
+
+    public class Metadata
+    {
+        [JsonProperty("id")]
+        public string id;
+
+        [JsonProperty("uri")]
+        public string uri;
+
+        [JsonProperty("type")]
+        public string type;
+    }
+
+    public class ResultsEsSalesOrderDataSet
+    {
+        [JsonProperty("__metadata")]
+        public Metadata __metadata;
+
+        [JsonProperty("Refobjkey")]
+        public string Refobjkey;
+
+        [JsonProperty("CreditExposureAmt")]
+        public string CreditExposureAmt;
+    }
+
+    public class ResultsItOrderItemsSet
+    {
+        [JsonProperty("__metadata")]
+        public Metadata __metadata;
+
+        [JsonProperty("DocNumber")]
+        public string DocNumber;
+
+        [JsonProperty("ItmNumber")]
+        public string ItmNumber;
+
+        [JsonProperty("ReqQty")]
+        public object ReqQty;
+
+        [JsonProperty("Material")]
+        public string Material;
+
+        [JsonProperty("SalesUnit")]
+        public string SalesUnit;
+
+        [JsonProperty("NetPrice")]
+        public object NetPrice;
+
+        [JsonProperty("Plant")]
+        public string Plant;
+
+        [JsonProperty("NetValue")]
+        public object NetValue;
+
+        [JsonProperty("Currency")]
+        public string Currency;
+
+        [JsonProperty("CondUnit")]
+        public string CondUnit;
+
+        [JsonProperty("GST")]
+        public object GST;
+
+        [JsonProperty("TotalPriceIncGST")]
+        public object TotalPriceIncGST;
+
+        [JsonProperty("Discount")]
+        public object Discount;
+    }
+
+    public class ResultsItOrderPartnersSet
+    {
+        [JsonProperty("__metadata")]
+        public Metadata __metadata;
+
+        [JsonProperty("RefObjKey")]
+        public string RefObjKey;
+
+        [JsonProperty("PartnRole")]
+        public string PartnRole;
+
+        [JsonProperty("PartnNumb")]
+        public string PartnNumb;
+
+        [JsonProperty("ItmNumber")]
+        public string ItmNumber;
+
+        [JsonProperty("PartnName")]
+        public string PartnName;
+
+        [JsonProperty("PartnCountry")]
+        public string PartnCountry;
+    }
+
+    public class ResultsItOrderSchedLinesSet
+    {
+        [JsonProperty("__metadata")]
+        public Metadata __metadata;
+
+        [JsonProperty("RefObjKey")]
+        public string RefObjKey;
+
+        [JsonProperty("ItmNumber")]
+        public string ItmNumber;
+
+        [JsonProperty("SchedLine")]
+        public string SchedLine;
+
+        [JsonProperty("ReqDate")]
+        public string ReqDate;
+
+        [JsonProperty("ReqQty")]
+        public string ReqQty;
+    }
+
+    public class EccPricingRequest
+    {
+        [JsonProperty("__metadata")]
+        public Metadata __metadata;
+
+        [JsonProperty("RefObjKey")]
+        public string RefObjKey;
+
+        [JsonProperty("DocType")]
+        public string DocType;
+
+        [JsonProperty("SalesOrg")]
+        public string SalesOrg;
+
+        [JsonProperty("DistrChan")]
+        public string DistrChan;
+
+        [JsonProperty("Division")]
+        public string Division;
+
+        [JsonProperty("SalesOff")]
+        public string SalesOff;
+
+        [JsonProperty("ReqDateH")]
+        public string ReqDateH;
+
+        [JsonProperty("PriceDate")]
+        public string PriceDate;
+
+        [JsonProperty("PurchNoC")]
+        public string PurchNoC;
+
+        [JsonProperty("CurrIso")]
+        public string CurrIso;
+
+        [JsonProperty("ItOrderItemsSet")]
+        public ItOrderItemsSet ItOrderItemsSet;
+
+        [JsonProperty("ItOrderPartnersSet")]
+        public ItOrderPartnersSet ItOrderPartnersSet;
+
+        [JsonProperty("ItOrderSchedLinesSet")]
+        public ItOrderSchedLinesSet ItOrderSchedLinesSet;
+
+        [JsonProperty("EsSalesOrderDataSet")]
+        public EsSalesOrderDataSet EsSalesOrderDataSet;
+    }
+
+
+}
---
+++ src/ecc-pricing/function/dulux.integration.ecc.models/Response/EccPricingResponse.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dulux.integration.ecc.models.Response
+{
+    public class Detail
+    {
+        public Metadata __metadata { get; set; }
+        public string RefObjKey { get; set; }
+        public string DocType { get; set; }
+        public string SalesOrg { get; set; }
+        public string DistrChan { get; set; }
+        public string Division { get; set; }
+        public string SalesOff { get; set; }
+        public string ReqDateH { get; set; }
+        public string PriceDate { get; set; }
+        public string PurchNoC { get; set; }
+        public string CurrIso { get; set; }
+        public ItOrderItemsSet ItOrderItemsSet { get; set; }
+        public ItOrderPartnersSet ItOrderPartnersSet { get; set; }
+        public ItOrderSchedLinesSet ItOrderSchedLinesSet { get; set; }
+        public EsSalesOrderDataSet EsSalesOrderDataSet { get; set; }
+    }
+
+    public class Deferred
+    {
+        public string uri { get; set; }
+    }
+
+    public class EsSalesOrderDataSet
+    {
+        public List<Result> results { get; set; }
+    }
+
+    public class IsOrderHeader
+    {
+        public Deferred __deferred { get; set; }
+    }
+
+    public class ItOrderItemsSet
+    {
+        public List<Result> results { get; set; }
+    }
+
+    public class ItOrderPartnersSet
+    {
+        public List<object> results { get; set; }
+    }
+
+    public class ItOrderSchedLinesSet
+    {
+        public List<object> results { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string id { get; set; }
+        public string uri { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Result
+    {
+        public Metadata __metadata { get; set; }
+        public string DocNumber { get; set; }
+        public string ItmNumber { get; set; }
+        public string ReqQty { get; set; }
+        public string Material { get; set; }
+        public string SalesUnit { get; set; }
+        public string NetPrice { get; set; }
+        public string Plant { get; set; }
+        public string NetValue { get; set; }
+        public string Currency { get; set; }
+        public string CondUnit { get; set; }
+        public string GST { get; set; }
+        public string TotalPriceIncGST { get; set; }
+        public string Discount { get; set; }
+        public IsOrderHeader IsOrderHeader { get; set; }
+        public string Refobjkey { get; set; }
+        public string NetValHd { get; set; }
+        public string GrossValHd { get; set; }
+        public string TaxAmountHd { get; set; }
+        public string CreditExposureAmt { get; set; }
+    }
+
+    public class EccPricingResponse
+    {
+        public Detail d { get; set; }
+    }
+}
