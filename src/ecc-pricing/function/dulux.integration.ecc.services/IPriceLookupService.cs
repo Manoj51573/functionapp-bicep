e24ec1800c569c9d648c779b6a5a59f88755696d
Manoj Kumar <Manoj.Kumar@insight.com>
2025-05-16 12:10:51 +0800

    Changed return type of function

--- src/ecc-pricing/function/dulux.integration.ecc.services/IPriceLookupService.cs
+++ src/ecc-pricing/function/dulux.integration.ecc.services/IPriceLookupService.cs
@@ -12,6 +12,6 @@ namespace dulux.integration.ecc.services
 {
     public interface IPriceLookupService
     {
-        Task<EccPricingResponse> GetPrice(EccPricingRequest pricingRequest);
+        Task<string> GetPrice(EccPricingRequest pricingRequest);
     }
 }
