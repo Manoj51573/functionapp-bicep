e42f46d0b86df0224bbf25f869b5a633847b5e18
Manoj Kumar <Manoj.Kumar@insight.com>
2025-05-22 11:05:35 +0800

    Removed SSL certification by-pass code

--- src/ecc-pricing/function/dulux.integration.ecc.pricing/Program.cs
+++ src/ecc-pricing/function/dulux.integration.ecc.pricing/Program.cs
@@ -8,27 +8,13 @@ using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 builder.Services
-   .AddApplicationInsightsTelemetryWorkerService()
-   .ConfigureFunctionsApplicationInsights();
-builder.Services.AddHttpClient(); // Default HttpClient
-builder.Services
-   .AddSingleton<IPriceLookupService, PriceLookupService>()
-   .AddOptions<SapEccOption>()
-       .Configure<IConfiguration>((sapEccOption, configuration) =>
-           configuration.GetSection(nameof(SapEccOption)).Bind(sapEccOption));
-builder.Services.AddHttpClient("UnsafeClient", client =>
-{
-    client.Timeout = TimeSpan.FromSeconds(60); // Adjust as needed
-})
-.ConfigurePrimaryHttpMessageHandler(() =>
-{
-    return new HttpClientHandler
-    {
-        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
-    };
-});
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights()
+    .AddHttpClient()
+    .AddSingleton<IPriceLookupService, PriceLookupService>()
+    .AddOptions<SapEccOption>()
+        .Configure<IConfiguration>((sapEccOption, configuration) => configuration.GetSection(nameof(SapEccOption)).Bind(sapEccOption));
+
 builder.Build().Run();
-
-
